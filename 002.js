be6fe31894887a8bdf7b85ba7a103538d7b0f61e
kzmmg <93984989+kzmmg@users.noreply.github.com>
2021-11-09 20:57:30 +0200

    Update 002.js

--- 002.js
+++ 002.js
@@ -1,3 +1,6 @@
+// Transcribing DNA into RNA
+// rosalind.info/problems/rna/
+
 const fs = require("fs")
 	
 fs.writeFileSync("answer.txt", fs.readFileSync("dataset.txt").toString().replace(/T/g, "U"))
