65607656d14c2ebac04e6f6565fd3ddc938640b2
kzmmg <93984989+kzmmg@users.noreply.github.com>
2021-11-09 14:36:21 +0200

    Update 004.js

--- 004.js
+++ 004.js
@@ -1,1 +1,11 @@
+const fs = require("fs")
+	
+let [n, k] = fs.readFileSync("dataset.txt").toString().split(" ")
 
+let fib3 = [1,1]
+
+for (let i = 2; i < n; i++) {
+	fib3[i] = fib3[i-2]*k + fib3[i-1]
+}
+
+fs.writeFileSync("answer.txt", fib3[n-1])
