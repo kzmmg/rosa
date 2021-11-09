478f2065996541cb73935c9e971fe12d8135642b
kzmmg <93984989+kzmmg@users.noreply.github.com>
2021-11-09 14:20:44 +0200

    Update 003.js

--- 003.js
+++ 003.js
@@ -1,1 +1,5 @@
-
+const fs = require("fs")
+	
+fs.writeFileSync("answer.txt", Array.
+	from(fs.readFileSync("dataset.txt").toString()).reverse().join("").replace(/T/g, "R").replace(/A/g, "T").
+	replace(/R/g, "A").replace(/C/g, "R").replace(/G/g, "C").replace(/R/g, "G"))
