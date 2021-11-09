16710cbd7a631bd59abc609bd9b340f06dae44ce
kzmmg <93984989+kzmmg@users.noreply.github.com>
2021-11-09 14:03:17 +0200

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,1 +1,15 @@
+const fs = require("fs")
 
+let dataset = fs.readFileSync("dataset.txt").toString()
+
+let acc = []
+acc["A"] = 0
+acc["C"] = 0
+acc["G"] = 0
+acc["T"] = 0
+
+let i
+for (i in dataset.split(""))
+	acc[dataset[i]]++
+	
+fs.writeFileSync("answer.txt", acc["A"] + " " + acc["C"] + " " + acc["G"] + " " + acc["T"])
