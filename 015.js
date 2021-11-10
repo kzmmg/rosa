af176060efec0170ea533ca7e3ef302e77d55682
kzmmg <93984989+kzmmg@users.noreply.github.com>
2021-11-10 05:39:16 +0200

    Update 015.js

--- 015.js
+++ 015.js
@@ -1,1 +1,18 @@
+// Finding a Motif in DNA 
+// rosalind.info/problems/subs/
 
+const fs = require("fs")
+
+let dataset = fs.readFileSync("dataset.txt").toString().split("\r\n")
+let dna = dataset[0]
+let motif = dataset[1]
+let lenm = motif.length
+	
+let pos = []
+for (i = 0; i < dna.length; i++) {
+	if (dna.substr(i,lenm) === motif)
+		pos.push(i + 1)
+}
+
+
+fs.writeFileSync("answer.txt", pos.join(" "))
