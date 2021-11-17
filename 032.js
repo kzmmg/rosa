2329d4ed434e2ef0d913906751711c85f906c475
kzmmg <93984989+kzmmg@users.noreply.github.com>
2021-11-17 12:09:07 +0200

    Update 032.js

--- 032.js
+++ 032.js
@@ -2,57 +2,57 @@
 // rosalind.info/problems/lgis/
 
 const fs = require("fs")
-const bi = require("big-integer")
 
 let dataset = fs.readFileSync("dataset.txt").toString().split("\r\n")
 
 let n = +dataset[0]
 let perm = dataset[1].split(" ").map(a=>+a)
 
-function fsub(alr, rest, op) {
-	if (rest.length == 0) {
-		return [alr]
-	}
-	
-	let last
-	let lena = alr.length
-	
-	if (lena) {
-		last = alr[lena-1]
-	} else {
-		last = op.max
-	}
-	
-	let i = 0
-	for (; i < rest.length; i++) {
-		if (op(rest[i], last)) {
-			break	
+function lss(op) {
+	let m = Array(perm.length + 1).fill(0)
+	let p = []
+	let l = 0
+	for (let i = 0; i < perm.length; i++) {
+		let cur = perm[i]
+		
+		let lo = 1
+		let hi = l + 1
+		
+		while (lo < hi) {
+			let mid = lo + Math.floor((hi - lo) / 2)
+			
+			if (op(perm[m[mid]],cur)) {
+				lo = mid + 1
+			} else {
+				hi = mid
+			}
 		}
+		
+		let newl = lo
+		
+		p[i] = m[lo - 1]
+		m[lo] = i
+		
+		if (newl > l) l = newl
+	}
+
+	let ss = []
+
+	let k = m[l]
+	//console.log(perm)
+	//console.log(l, k)
+	//console.log(m)
+	//console.log(p)
+	for (let i = l - 1; i >= 0; i--) {
+		ss[i] = perm[k]
+		k = p[k]
 	}
 	
-	if (i >= rest.length) {
-		return [alr]
-	}
-	
-	let nextrest = rest.slice(i + 1)
-	let alr1 = alr.concat([])
-	let alr2 = alr.concat([])
-	
-	alr1.push(rest[i])
-	
-	return fsub(alr1, nextrest, op).concat(fsub(alr2, nextrest, op))
-	
+	return ss
 }
 
-let mo = (a, b) => a > b
-let le = (a, b) => a < b
+let inc = lss((a,b) => a < b)
+let dec = lss((a,b) => a > b)
 
-mo.max = -1
-le.max = Math.pow(2,32)
-
-let inc = fsub([], perm, mo).reduce((a, i) => i.length > a.length ? i : a, [])
-let dec = fsub([], perm, le).reduce((a, i) => i.length > a.length ? i : a, [])
-
-
-
+//console.log(dec)
 fs.writeFileSync("answer.txt", inc.join(" ") + "\n" + dec.join(" "))
